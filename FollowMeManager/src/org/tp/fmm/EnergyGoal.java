5752ac36a1edf679cef03423fcc311477f8b50de
ptphucbk <ptphucbk@gmail.com>
2016-01-19 08:56:30 +0000

    Commit 19/01/2016

---
+++ FollowMeManager/src/org/tp/fmm/EnergyGoal.java
@@ -0,0 +1,23 @@
+package org.tp.fmm;
+
+public enum EnergyGoal {
+	LOW(100d), MEDIUM(250d), HIGH(1000d);
+
+	/**
+	 * The corresponding maximum energy in watt
+	 */
+	private double maximumEnergyInRoom;
+
+	/**
+	 * get the maximum energy consumption in each room
+	 * 
+	 * @return the energy in watt
+	 */
+	public double getMaximumEnergyInRoom() {
+		return maximumEnergyInRoom;
+	}
+
+	private EnergyGoal(double powerInWatt) {
+		maximumEnergyInRoom = powerInWatt;
+	}
+}
